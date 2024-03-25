8b88b1b8e8edef34607c6f25723d908cff079d5e
Blade <bladeprojectz@gmail.com>
2024-03-25 08:19:14 +0800

    lab complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
-// Code EyesOnMe Component Here
+// Code EyesOnMe Component Her
+import React from "react";
+
+function handleOnFocus() {
+  console.log("Good!");
+}
+
+function handleOnBlur() {
+  console.log("Hey! Eyes on me!");
+}
+
+function EyesOnMe() {
+  return (
+    <div>
+      <button
+        onFocus={() => {
+          console.log("Good!");
+        }}
+        onBlur={handleOnBlur}
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
 // Code Keypad Component Here
+import React, { useState } from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const [password, setPassword] = useState("");
+  return (
+    <div>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+          console.log("Entering password...");
+        }}
+      />
+    </div>
+  );
 }
 
 export default Keypad;
